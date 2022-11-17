1c69f827ea116a4e33e4d187695369081919dea4
Evan Mitchell <evanm4183@gmail.com>
2022-11-17 17:33:27 -0600

    added method to insert list into tree

--- binarytree/tree.py
+++ binarytree/tree.py
@@ -4,6 +4,7 @@ class Tree:
     def __init__(self, val):
         self.root = Leaf(val)
 
+    # method for inserting a number into the tree
     def insert(self, val):
         def insert_leaf(curr_leaf, val):
             if val == curr_leaf.val:
@@ -18,3 +19,8 @@ class Tree:
                 insert_leaf(curr_leaf.right, val)
 
         insert_leaf(self.root, val)
+
+    # method for inserting a list of numbers into the tree
+    def insert_list(self, list):
+        for num in list:
+            self.insert(num)
--- main.py
+++ main.py
@@ -4,11 +4,5 @@ from binarytree.leaf import Leaf
 new_tree = Tree(10)
 new_tree.root.display_leaf()
 
-new_tree.insert(11)
+new_tree.insert_list([5, 15])
 new_tree.root.display_leaf()
-
-new_tree.insert(9)
-new_tree.root.display_leaf()
-
-new_tree.insert(15)
-new_tree.root.right.display_leaf()
