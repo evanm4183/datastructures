c59c479e6476a22b56b85dd2c0ff14b96a701f9c
Evan Mitchell <evanm4183@gmail.com>
2022-11-17 17:25:21 -0600

    made insert for binary tree

--- binarytree/tree.py
+++ binarytree/tree.py
@@ -3,3 +3,18 @@ from binarytree.leaf import Leaf
 class Tree:
     def __init__(self, val):
         self.root = Leaf(val)
+
+    def insert(self, val):
+        def insert_leaf(curr_leaf, val):
+            if val == curr_leaf.val:
+                return
+            elif val < curr_leaf.val and curr_leaf.left is None:
+                curr_leaf.left = Leaf(val)
+            elif val > curr_leaf.val and curr_leaf.right is None:
+                curr_leaf.right = Leaf(val)
+            elif val < curr_leaf.val:
+                insert_leaf(curr_leaf.left, val)
+            elif val > curr_leaf.val:
+                insert_leaf(curr_leaf.right, val)
+
+        insert_leaf(self.root, val)
--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from binarytree.leaf import Leaf
 
 new_tree = Tree(10)
 new_tree.root.display_leaf()
+
+new_tree.insert(11)
+new_tree.root.display_leaf()
+
+new_tree.insert(9)
+new_tree.root.display_leaf()
+
+new_tree.insert(15)
+new_tree.root.right.display_leaf()
